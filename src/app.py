fdeffe45d9f97b285bbcbe8d760e7c9f72964ae2
alfarizi <farizi1906@gmail.com>
2023-05-21 23:20:05 +0700

    update app.py

--- src/app.py
+++ src/app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",index=True)
 
 @app.route("/result")
 def result():
-    return render_template("result.html")
+    return render_template("result.html",result=True)
 
 @app.route("/input",methods=["GET","POST"])
 def _input():
-    return render_template("input.html")
+    return render_template("input.html",_input=True)
 
 
 app.run(debug=True,port=8000)
